79dd1b9f3aa12be253c9104b573ba6249543d300
ryanfelicilda <ryan.g.felicilda@gmil.com>
2019-09-11 12:50:15 -0500

    Added CustomList Class. Added potential test in CustomListTest Class.

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        private T[] items;
+        public CustomList()
+        {
+            items = new T[5];
+        }
+    }
+}
---
+++ CustomListTest/CustomListTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void Method_Condition_ExpectedResult()
+        {
+            // Condition to test:
+
+
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+
+        }
+        [TestMethod]
+        public void Add_NewItemToEmptyList_ItemAddedToIndexZero()
+        {
+            // Condition to test:
+            // Add an item to an empty list and new item will be in [0] index.
+
+            // Arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 5;
+            int actual;
+
+            // Act
+            test.Add(5);
+            actual = test[0];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        public void Add_NewItemToEmptyList_ItemCountIncreaseOneIncrement()
+        {
+            // Condition to test:
+            // Add an item to an empty list and item count increases one increment.
+
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+
+        }
+        public void Add_NewItemToExistingList_ItemAddedToEndOfList()
+        {
+            // Condition to test:
+            // Add an item to an existing list and new item is added at the end of the list.
+
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+
+        }
+        public void Add_NewItemToExistingList_ItemCountIncreaseOneIncrement()
+        {
+            // Condition to test:
+            // Add an item to an existing list and item count increases one increment.
+
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+
+        }
+    }
+}
